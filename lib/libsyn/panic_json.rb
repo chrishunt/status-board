ebe1993ca78ac6763ff8ba188aee3c36a187bca7
Chris Hunt <chrishunt@github.com>
2014-08-03 21:25:33 -0700

    Add #to_s to json for returning everything

--- lib/libsyn/panic_json.rb
+++ lib/libsyn/panic_json.rb
@@ -48,26 +48,32 @@ module Libsyn
       }.to_json
     end
 
+    def to_s
+      [most_recent, totals].join "\n"
+    end
+
     private
 
     def parse(input)
-      downloads = Hash.new { |h, k| h[k] = [] }
-      episodes  = []
+      @parse ||= begin
+        downloads = Hash.new { |h, k| h[k] = [] }
+        episodes  = []
 
-      months = input.dup.shift.slice(2..-2).map do |month|
-        month.gsub("downloads__", "").capitalize
+        months = input.dup.shift.slice(2..-2).map do |month|
+          month.gsub("downloads__", "").capitalize
+        end
+
+        input.each do |line|
+          episode, _, *dls = *line
+          next if episode.split(':').count == 1
+
+          episodes << episode
+
+          0.upto(months.count).each { |i| downloads[months[i]] << dls[i].to_i }
+        end
+
+        [episodes, downloads, months]
       end
-
-      input.each do |line|
-        episode, _, *dls = *line
-        next if episode.split(':').count == 1
-
-        episodes << episode
-
-        0.upto(months.count).each { |i| downloads[months[i]] << dls[i].to_i }
-      end
-
-      [episodes, downloads, months]
     end
   end
 end
