1849fc7a3ed626112489b150f929cdbef9721c81
Chris Hunt <chrishunt@github.com>
2014-08-05 15:24:32 -0700

    Move test helper closer to the tests it's helping

--- test/status_board/libsyn_test.rb
+++ test/status_board/libsyn_test.rb
@@ -3,29 +3,6 @@ require 'status_board/libsyn'
 
 module StatusBoard
   describe Libsyn do
-    def libsyn_stats
-      [[
-        "item_title",
-        "release_date",
-        "downloads__august",
-        "downloads__july",
-        "downloads__june",
-        "downloads__total"
-      ],[
-        "3: Most recent", "2014-08-02 00:52:24",
-        "10", "11", "12", "33"
-      ],[
-        "2: This is a test", "2014-08-01 00:52:24",
-        "7", "8", "9", "24"
-      ],[
-        "1: Why I use Vim", "2014-07-30 00:52:24",
-        "4", "5", "6", "15"
-      ],[
-        "2", "2014-07-25 04:33:45",
-        "1", "2", "3", "6"
-      ]]
-    end
-
     describe '#get' do
       it 'gets a CSV' do
         VCR.use_cassette('get') do
@@ -48,6 +25,29 @@ module StatusBoard
           ]], Libsyn.new('me@example.com', 'secret', '1234').get
         end
       end
+    end
+
+    def libsyn_stats
+      [[
+        "item_title",
+        "release_date",
+        "downloads__august",
+        "downloads__july",
+        "downloads__june",
+        "downloads__total"
+      ],[
+        "3: Most recent", "2014-08-02 00:52:24",
+        "10", "11", "12", "33"
+      ],[
+        "2: This is a test", "2014-08-01 00:52:24",
+        "7", "8", "9", "24"
+      ],[
+        "1: Why I use Vim", "2014-07-30 00:52:24",
+        "4", "5", "6", "15"
+      ],[
+        "2", "2014-07-25 04:33:45",
+        "1", "2", "3", "6"
+      ]]
     end
 
     describe '#totals' do
