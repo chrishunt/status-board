fa4048dac0d5edd47380c92ea5c5d7c1cfb61eb4
Chris Hunt <chrishunt@github.com>
2014-08-08 09:17:12 -0700

    Move config to its own file

--- app.rb
+++ app.rb
@@ -1,14 +1,8 @@
 require 'sinatra'
 
+require_relative 'lib/status_board/config'
 require_relative 'lib/status_board/libsyn'
 require_relative 'lib/status_board/chartbeat'
-
-CHARTBEAT_API_KEY = ENV['CHARTBEAT_API_KEY']
-CHARTBEAT_DOMAIN  = ENV['CHARTBEAT_DOMAIN']
-
-LIBSYN_EMAIL    = ENV['LIBSYN_EMAIL']
-LIBSYN_PASSWORD = ENV['LIBSYN_PASSWORD']
-LIBSYN_SHOW_ID  = ENV['LIBSYN_SHOW_ID']
 
 def libsyn
   StatusBoard::Libsyn.new LIBSYN_EMAIL, LIBSYN_PASSWORD, LIBSYN_SHOW_ID
